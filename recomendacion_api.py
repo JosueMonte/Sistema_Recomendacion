a051a75749b1d1b2708c659846628aa15566843a
JosueMonte <josue.monte@outlook.com>
2024-10-28 08:27:33 -0300

    endpoints1

--- recomendacion_api.py
+++ recomendacion_api.py
@@ -1,6 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import pandas as pd
 
 app = FastAPI()
+
+# Cargar el DataFrame
+df = pd.read_csv('data_movies.csv')
+
+# Mapeo de meses
+month_mapping = {
+    "enero": 1, "febrero": 2, "marzo": 3, "abril": 4,
+    "mayo": 5, "junio": 6, "julio": 7, "agosto": 8,
+    "septiembre": 9, "octubre": 10, "noviembre": 11, "diciembre": 12
+}
+
+
+def film_month(df, month_name):
+    # Convertir el nombre del mes a su número correspondiente
+    month_number = month_mapping.get(month_name.lower())
+    if month_number is None:
+        raise ValueError(f"Mes '{month_name}' no es válido.")
+    # Filtrar las filas que corresponden al mes especificado
+    filtered_df = df[df['month'] == month_number]
+    # Contar los IDs en las filas filtradas
+    count_ids = filtered_df['id'].count()
+    return {"endpoint1": f"Fueron estrenadas {count_ids} peliculas en el mes de {month_name}"}
+
+
+class MonthRequest(BaseModel):
+    month_name: str
+
+
+@app.post("/film_month_count")
+async def film_month_count(request: MonthRequest):
+    try:
+        resultado = film_month(df, request.month_name)
+        return resultado
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @app.get('/')
