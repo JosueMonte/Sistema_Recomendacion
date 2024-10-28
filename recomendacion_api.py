477694857ac9e4a948623d8e14a3f44774778c8d
JosueMonte <josue.monte@outlook.com>
2024-10-28 15:46:18 -0300

    correccion endpoint1

--- recomendacion_api.py
+++ recomendacion_api.py
@@ -15,14 +15,11 @@ month_mapping = {
 
 def film_count_m(df, month: str):
     """Obtiene la cantidad de filmaciones según mes indicado
-
     Args:
         df: DataFrame de películas 
         month (str): nombre del mes en castellano
-
     Raises:
         HTTPException: en caso de que el mes indicado no sea válido o no esté escrito en castellano o sea nulo
-
     Returns:
         _type_: entero o integer
     """
@@ -31,10 +28,8 @@ def film_count_m(df, month: str):
     if month_number is None:
         raise HTTPException(
             status_code=400, detail=f"Mes '{month}' no es válido.")
-
     # Filtrar las filas que corresponden al mes especificado
     filtered_df = df[df['month'] == month_number]
-
     # Contar los IDs en las filas filtradas
     count_ids = filtered_df['id'].count()
     return {"endpoint0": f"Fueron estrenadas {count_ids} peliculas en el mes de {month}"}
@@ -43,20 +38,16 @@ def film_count_m(df, month: str):
 @app.get('/film_count_m/{month}')
 async def film_cant_mes(month: str):
     """Obtiene la cantidad de filmaciones según mes desde un archivo en formato csv
-
     Args:
         month (str): ejemplo: "enero"
-
     Returns:
         _type_: ejemplo: 2000 filmaciones en el mes de enero
     """
     try:
         # Cambia la ruta del archivo según la ubicación de tu archivo
         df = pd.read_csv("dataset/data_movies.csv")
-
         # Aplicar la función para obtener la cantidad de filmaciones según mes aplicado
         result = film_count_m(df, month)
-
         return JSONResponse(content=jsonable_encoder(result), media_type="application/json")
     except FileNotFoundError:
         raise HTTPException(
@@ -65,7 +56,6 @@ async def film_cant_mes(month: str):
         raise HTTPException(
             status_code=500, detail=f"Error al leer el archivo csv: {str(e)}")
 
-
-@app.get('/')
-def read_root():
-    return {"welcome": "Bienvenido a la API"}
+if __name__ == "_recomendacion_api__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
