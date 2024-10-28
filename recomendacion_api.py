3263284158e6fdd73b847c230fb8a51899a5a6c9
JosueMonte <josue.monte@outlook.com>
2024-10-28 09:32:41 -0300

    correccion endpoint1

--- recomendacion_api.py
+++ recomendacion_api.py
@@ -3,52 +3,6 @@ import pandas as pd
 
 app = FastAPI()
 
-
-@app.get('/')
-def read_root():
-    return {"welcome": "Bienvenido a la API"}
-
-
-@app.get('/cantidad_filmaciones_mes')
-def cantidad_filmaciones_mes(mes: str):
-    return {"endpoint1": f"Fueron estrenadas 1000 peliculas en el mes de {mes}"}
-
-
-@app.get('/cantidad_filmaciones_dia')
-def cantidad_filmaciones_dia(dia: str):
-    return {"endpoint2": f"Fueron estrenadas 1000 peliculas los días {dia}"}
-
-
-@app.get('/score_titulo')
-def score_titulo(titulo: str):
-    return {"endpoint3": f"La película {titulo} fue estrenada en el año 1000 con un puntaje de 100"}
-
-
-@app.get('/votos_titulo')
-def votos_titulo(titulo: str):
-    return {"endpoint4": f"La película {titulo} fue estrenada en el año Y, cuenta con un total de Y valoraciones y un promedio de Z"}
-
-
-@app.get('/get_actor')
-def get_actor(nombre_actor: str):
-    return {"endpoint5": f"El actor {nombre_actor} ha participado de Y cantidad de filmaciones, ha conseguido un retorno de Y y un promedio de retorno de X por filmación"}
-
-
-@app.get('/get_director')
-def get_director(nombre_director: str):
-    return {"endpoint6": f"El director {nombre_director} ha conseguido un retorno de Y, y sus películas con su respectiva fecha de lanzamiento, retorno individual, costo y ganancia son las siguientes:"}
-
-
-@app.get('/recomendacion')
-def recomendacion(titulo: str):
-    return {'endpoints7': f'Te recomiendo mirar estas películas similares:',
-            'lista de películas:': ['película1',
-                                    'película2',
-                                    'película3',
-                                    'película4',
-                                    'película5']}
-
-
 # Cargar el DataFrame
 df = pd.read_csv('data_movies.csv')
 
@@ -65,8 +19,8 @@ def film_count_m(month: str):
     # Convertir el nombre del mes a su número correspondiente
     month_number = month_mapping.get(month.lower())
     if month_number is None:
-        raise ValueError(f"Mes '{month}' no es válido.")
-
+        raise HTTPException(
+            status_code=400, detail=f"Mes '{month}' no es válido.")
     # Filtrar las filas que corresponden al mes especificado
     filtered_df = df[df['month'] == month_number]
     # Contar los IDs en las filas filtradas
@@ -74,6 +28,6 @@ def film_count_m(month: str):
     return {"endpoint0": f"Fueron estrenadas {count_ids} peliculas en el mes de {month}"}
 
 
-if __name__ == "__main__":
+if __name__ == "__recomendacion_api__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
